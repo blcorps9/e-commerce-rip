b7e171f964eea0a732a8ab5bf7b19e3eca57f9a0
Saint Walker <blcorps9@gmail.com>
2020-06-01 16:48:42 +0530

    S15: Payment Page - Form validation, add useState hook

--- src/pages/PaymentPage/index.jsx
+++ src/pages/PaymentPage/index.jsx
@@ -1,131 +1,173 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import _map from "lodash/map";
 import _range from "lodash/range";
+import _isEmpty from "lodash/isEmpty";
 
 import CardForm from "../../components/CardForm";
 
 import { saveNewCard, updateCard, deleteCard } from "./actions";
 
-class PaymentPage extends Component {
-  constructor(props) {
-    super(props);
+import { isValidCardNumber } from "../../utils";
 
-    this.state = {
-      showAddCardForm: false,
-      updateCardForm: null,
-    };
-  }
+function PaymentPage(props) {
+  const [showAddCardForm, setShowAddCardForm] = useState(false);
+  const [updateCardForm, setUpdateCardForm] = useState(null);
+  const [formError, setFormError] = useState({
+    inputCardHolderName: "",
+    inputCardNumber: "",
+    inputExpiryMonth: "",
+    inputExpiryYear: "",
+    inputCVV: "",
+  });
 
-  onClickShowCardForm = (e) => {
-    e.preventDefault();
-    e.stopPropagation();
-
-    this.setState({ showAddCardForm: true, updateCardForm: null });
-  };
-
-  onClickDelete = (e) => {
+  const onClickUpdate = (e) => {
     e.preventDefault();
     e.stopPropagation();
 
     const cardKey = e.currentTarget.getAttribute("data-key");
-
-    this.props.deleteCard({ key: cardKey });
+    setUpdateCardForm(cardKey);
+    setShowAddCardForm(false);
   };
 
-  onClickUpdate = (e) => {
+  const onClickShowCardForm = (e) => {
     e.preventDefault();
     e.stopPropagation();
 
-    const cardKey = e.currentTarget.getAttribute("data-key");
-
-    this.setState({ updateCardForm: cardKey });
+    setShowAddCardForm(true);
+    setUpdateCardForm(null);
   };
 
-  onSubmit = (e) => {
+  const onSubmit = (e) => {
     e.preventDefault();
     e.stopPropagation();
 
-    const formData = { key: this.state.updateCardForm };
+    const formData = { key: updateCardForm };
+    const formErrors = {};
 
     for (let field of e.currentTarget) {
       if (field.type !== "submit") {
-        formData[field.id] =
-          field.type === "checkbox" ? field.checked : field.value;
+        switch (field.id) {
+          case "inputCardHolderName":
+            formData[field.id] = field.value;
+            if (field.value.length === 0) {
+              formErrors.inputCardHolderName = "Please provide name";
+            }
+            break;
+          case "inputCardNumber":
+            formData[field.id] = field.value.replace(/[^\d]/g, "");
+            if (!isValidCardNumber(field.value)) {
+              formErrors.inputCardNumber = "Please provide valid card number";
+            }
+            break;
+          case "inputExpiryMonth":
+            formData[field.id] = field.value;
+            if (field.value.length === 0) {
+              formErrors.inputExpiryMonth = "Please provide valid expiry month";
+            }
+            break;
+          case "inputExpiryYear":
+            formData[field.id] = field.value;
+            if (field.value.length === 0) {
+              formErrors.inputExpiryYear = "Please provide valid expiry year";
+            }
+            break;
+          case "inputCVV":
+            formData[field.id] = field.value;
+            if (field.value.length !== 3) {
+              formErrors.inputCVV = "Please provide valid CVV number";
+            }
+            break;
+          default:
+            formData[field.id] =
+              field.type === "checkbox" ? field.checked : field.value;
+        }
       }
     }
 
-    this.setState({ showAddCardForm: false, updateCardForm: null }, () => {
-      formData.key
-        ? this.props.updateCard(formData)
-        : this.props.saveNewCard(formData);
-    });
+    if (_isEmpty(formErrors)) {
+      setShowAddCardForm(false);
+      setUpdateCardForm(null);
+
+      formData.key ? props.updateCard(formData) : props.saveNewCard(formData);
+    } else {
+      setFormError(formErrors);
+    }
   };
 
-  render() {
-    const { myCards } = this.props;
-    const { showAddCardForm, updateCardForm } = this.state;
+  const onClickDelete = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
 
-    return (
-      <div className="cart-page row mb-4">
-        <div className="col-10 offset-1">
-          <div className="card">
-            <div className="card-header">Payment Methods</div>
-            <ul className="list-group list-group-flush">
-              {myCards.map((card) => (
-                <li className="list-group-item" key={card.key}>
-                  {`${card.inputCardHolderName}, ${card.inputCardNumber}`}
-                  <br />
+    const cardKey = e.currentTarget.getAttribute("data-key");
 
-                  {updateCardForm === card.key ? (
-                    <CardForm onSubmit={this.onSubmit} card={card} />
-                  ) : (
-                    <>
-                      <button
-                        className="btn btn-secondary float-right"
-                        onClick={this.onClickDelete}
-                        data-key={card.key}
-                        disabled={card.inputSaveCard ? "disabled" : null}
-                      >
-                        Delete
-                      </button>
-                      <button
-                        className="btn btn-primary float-right mr-1"
-                        onClick={this.onClickUpdate}
-                        data-key={card.key}
-                      >
-                        Update
-                      </button>
-                    </>
-                  )}
-                </li>
-              ))}
-              {showAddCardForm ? (
-                <li className="list-group-item">
-                  <CardForm onSubmit={this.onSubmit} isNew />
-                </li>
-              ) : (
-                <li className="list-group-item actions d-flex align-items-end">
-                  <span
-                    className="btn btn-primary"
-                    onClick={this.onClickShowCardForm}
-                  >
-                    Add new card
-                  </span>
-                </li>
-              )}
-            </ul>
-          </div>
-        </div>
-        <div className="col-1 offset-10 mt-2">
-          <Link className="btn btn-primary" to="/">
-            Pay
-          </Link>
+    props.deleteCard({ key: cardKey });
+  };
+
+  const { myCards } = props;
+
+  return (
+    <div className="cart-page row mb-4">
+      <div className="col-10 offset-1">
+        <div className="card">
+          <div className="card-header">Payment Methods</div>
+          <ul className="list-group list-group-flush">
+            {myCards.map((card) => (
+              <li className="list-group-item" key={card.key}>
+                {`${
+                  card.inputCardHolderName
+                }, XXX*******XX${card.inputCardNumber.substr(-4)}`}
+                <br />
+
+                {updateCardForm === card.key ? (
+                  <CardForm
+                    onSubmit={onSubmit}
+                    card={card}
+                    formError={formError}
+                  />
+                ) : (
+                  <>
+                    <button
+                      className="btn btn-secondary float-right"
+                      onClick={onClickDelete}
+                      data-key={card.key}
+                      disabled={card.inputSaveCard ? "disabled" : null}
+                    >
+                      Delete
+                    </button>
+                    <button
+                      className="btn btn-primary float-right mr-1"
+                      onClick={onClickUpdate}
+                      data-key={card.key}
+                    >
+                      Update
+                    </button>
+                  </>
+                )}
+              </li>
+            ))}
+            {showAddCardForm ? (
+              <li className="list-group-item">
+                <CardForm onSubmit={onSubmit} isNew formError={formError} />
+              </li>
+            ) : (
+              <li className="list-group-item actions d-flex align-items-end">
+                <span className="btn btn-primary" onClick={onClickShowCardForm}>
+                  Add new card
+                </span>
+              </li>
+            )}
+          </ul>
         </div>
       </div>
-    );
-  }
+      <div className="col-1 offset-10 mt-2">
+        <Link className="btn btn-primary" to="/">
+          Pay
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default connect((state) => ({ myCards: state.myCards.data }), {
--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,5 @@
 import _sampleSize from "lodash/sampleSize";
+import _chunk from "lodash/chunk";
 
 // https://stackoverflow.com/questions/105034/how-to-create-guid-uuid#answer-2117523
 export const uuidv4 = () => {
@@ -91,4 +92,49 @@ export function getSorted(data = [], prop, order = "asc") {
 
     return 0;
   });
+}
+
+// https://en.wikipedia.org/wiki/Luhn_algorithm
+export function isValidCardNumber(num = "") {
+  const digits = String(num)
+    .replace(/[^0-9]/g, "")
+    .split("")
+    .map(Number);
+
+  const len = digits.length;
+  const parity = len % 2;
+  let sum = digits[len - 1];
+
+  for (let i = len - 2; i > -1; i--) {
+    let d = digits[i];
+
+    if (i % 2 === parity) {
+      d *= 2;
+
+      if (d > 9) d -= 9;
+    }
+
+    sum += d;
+  }
+
+  return sum % 10 === 0;
+}
+
+// e.g. i/p 4111111111111111
+//      o/p 4111 1111 1111 1111
+export function maskCardNumber(num) {
+  // let masked = '';
+  // for (let i = 0; i < num.length; i++) {
+  //   if ((i + 1 !== num.length && (i + 1) % 4) === 0) {
+  //     masked += num[i] + " ";
+  //   } else {
+  //     masked += num[i];
+  //   }
+  // }
+  // return masked;
+
+  // lodash
+  return _chunk(num, 4)
+    .map((a) => a.join(""))
+    .join(" ");
 }
